9ced03d534ecf1b4882dd8c4fdf5724e3691a912
danibene <34680344+danibene@users.noreply.github.com>
2024-03-25 07:57:32 -0400

    add test for generation of filename

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,11 +1,44 @@
-from avcap.skeleton import main
+from pytest import CaptureFixture
+
+from avcap.skeleton import generate_filename, main
 
 __author__ = "danibene"
 __copyright__ = "danibene"
 __license__ = "MIT"
 
 
-def test_main(capsys):
+def test_generate_filename() -> None:
+    """Test the generate_filename function"""
+    filename = generate_filename()
+    assert filename.startswith("preprocessed_")
+    assert filename.endswith(".mp4")
+
+    filename = generate_filename("test.mp4")
+    assert filename.startswith("preprocessed_test_")
+    assert filename.endswith(".mp4")
+
+    filename = generate_filename("test")
+    assert filename.startswith("preprocessed_test_")
+    assert filename.endswith(".mp4")
+
+    filename = generate_filename("test.avi")
+    assert filename.startswith("preprocessed_test_")
+    assert filename.endswith(".avi")
+
+    filename = generate_filename("test/test.avi")
+    assert filename.startswith("test/preprocessed_test_")
+    assert filename.endswith(".avi")
+
+    filename = generate_filename("test/test")
+    assert filename.startswith("test/preprocessed_test_")
+    assert filename.endswith(".mp4")
+
+    filename = generate_filename("test/test.mp4")
+    assert filename.startswith("test/preprocessed_test_")
+    assert filename.endswith(".mp4")
+
+
+def test_main(capsys: CaptureFixture) -> None:
     """CLI Tests"""
     # capsys is a pytest fixture that allows asserts against stdout/stderr
     # https://docs.pytest.org/en/stable/capture.html
